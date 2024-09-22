d2f47f299e2ffb95d81f61d104c488dd4fb15071
zainah7 <zainaabd7@gmail.com>
2024-09-22 19:55:27 +0100

    Added contact us and store list

---
+++ src/Components/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import map from "../Images/Screenshot 2024-09-17 at 5.30.42 PM.png";
+
+function Map() {
+  return (
+    <div>
+      <img src={map} alt="" className="w-full m-6" />
+    </div>
+  );
+}
+
+export default Map;
